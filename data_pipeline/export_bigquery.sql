494c9466f66edd5d3fbc41ede00f430cbc4cf06e
zxhuang-Ovo <zixuan.huang.22@alumni.ucl.ac.uk>
2026-04-10 18:11:19 +0800

    add BigQuery export SQL for ICU data pipeline

--- data_pipeline/export_bigquery.sql
+++ data_pipeline/export_bigquery.sql
@@ -1,1 +1,15 @@
+-- Export vitals from BigQuery to GCS (Google Cloud Storage)
+EXPORT DATA OPTIONS(
+  uri='gs://icu-data-export-2026/vitals/vitals-*.parquet',
+  format='PARQUET'
+)
+AS
+SELECT * FROM dataset.vitals_long_48h;
 
+-- Export labs from BigQuery to GCS
+EXPORT DATA OPTIONS(
+  uri='gs://icu-data-export-2026/labs/labs-*.parquet',
+  format='PARQUET'
+)
+AS
+SELECT * FROM dataset.labs_long_48h;
