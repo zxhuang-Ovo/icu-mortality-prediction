7829b50aa8dafb5f5eefa057aa8c8c7311576b31
zxhuang-Ovo <zixuan.huang.22@alumni.ucl.ac.uk>
2026-04-10 18:13:17 +0800

    add GCS download script using gsutil

--- data_pipeline/download_from_gcs.sh
+++ data_pipeline/download_from_gcs.sh
@@ -1,1 +1,5 @@
+mkdir -p ~/Downloads/vitals
+mkdir -p ~/Downloads/labs
 
+gsutil -m cp "gs://icu-data-export-2026/vitals/*" ~/Downloads/vitals/
+gsutil -m cp "gs://icu-data-export-2026/labs/*" ~/Downloads/labs/
